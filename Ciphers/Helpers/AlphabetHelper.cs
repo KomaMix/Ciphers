6e7ae2e68306dd4796a96c788a293abfe4f2edeb
KomaMix <ksabirov448@gmail.com>
2024-11-27 18:05:48 +0300

    added AlphaHelper

---
+++ Ciphers/Helpers/AlphabetHelper.cs
@@ -0,0 +1,38 @@
+﻿using Ciphers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciphers.Helpers
+{
+    public static class AlphabetHelper
+    {
+        private static readonly string _alphaRus = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+        private static readonly string _alhaEng = "abcdefghijklmnopqrstuvwxyz";
+
+
+        public static string GetEnglishLowerAlpha()
+        {
+            return _alhaEng;
+        }
+
+        public static string GetRussianLowerAlpha()
+        {
+            return _alphaRus;
+        }
+
+        public static string GetLowerAlphaForLanguageType(TextLanguage language)
+        {
+            if (language == TextLanguage.English)
+            {
+                return GetEnglishLowerAlpha();
+            }
+            else
+            {
+                return GetRussianLowerAlpha();
+            }
+        }
+    }
+}
