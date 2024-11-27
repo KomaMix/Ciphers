4981cc73491d5b47efc42fe29f7ab6750d4ebdb0
KomaMix <ksabirov448@gmail.com>
2024-11-27 18:58:12 +0300

    added LanguageHelper

---
+++ Ciphers/Helpers/LanguageHelper.cs
@@ -0,0 +1,55 @@
+﻿using Ciphers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Ciphers.Helpers
+{
+    public class LanguageHelper
+    {
+        public static TextLanguage GetLanguageType(string content)
+        {
+            if (Enum.TryParse(content, out TextLanguage selectedLanguage))
+            {
+                return selectedLanguage;
+            }
+
+            throw new InvalidOperationException("Невозможно преобразовать выбранный элемент в EncryptLanguage");
+        }
+
+        public static TextLanguage GetLanguageTypeForComboBox(Object item)
+        {
+            var comboBox = item as ComboBox;
+
+            var comboBoxItem = comboBox.SelectedItem as ComboBoxItem;
+
+
+            if (comboBoxItem != null)
+            {
+                string selectedContent = comboBoxItem.Content.ToString();
+
+                return GetLanguageType(selectedContent);
+            }
+
+            throw new InvalidOperationException("Невозможно преобразовать выбранный элемент в EncryptLanguage");
+        }
+
+        public static bool CheckTextIsAlphabet(string text, TextLanguage language)
+        {
+            string alpha = AlphabetHelper.GetLowerAlphaForLanguageType(language);
+
+            foreach (char ch in text.ToLower())
+            {
+                if (!alpha.Contains(ch))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
