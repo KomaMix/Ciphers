2649bc2d2fda374121c4dcb84088fd9e9a8ca6e0
KomaMix <ksabirov448@gmail.com>
2024-11-27 21:51:41 +0300

    added GammaGeneratorService

---
+++ Ciphers/Services/GammaGeneratorService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciphers.Services
+{
+    public static class GammaGeneratorService
+    {
+        private static readonly Random _random = new Random();
+
+        public static bool[] GenerateRandomGamma(int length)
+        {
+            if (length <= 0)
+                throw new ArgumentException("Длина гаммы должна быть положительным числом.");
+
+            bool[] gamma = new bool[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                gamma[i] = _random.Next(2) == 1;
+            }
+
+            return gamma;
+        }
+    }
+}
